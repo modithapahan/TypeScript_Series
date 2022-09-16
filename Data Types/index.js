c85200f3110e4c64d1894f2fe21b9620acf6969b
modithapahan <modithapahan0@gmail.com>
2022-09-16 22:09:31 +0530

    Type Script

---
+++ Data Types/index.js
@@ -0,0 +1,15 @@
+//Number
+//String
+//boolean
+//void
+//null
+//undefined
+/* Number */
+var no1 = 12;
+console.log(no1);
+/* String */
+var name1 = "Moditha";
+console.log(name1);
+/* Null */
+var age = null;
+console.log(age);
---
+++ Data Types/index.ts
@@ -0,0 +1,18 @@
+//Number
+//String
+//boolean
+//void
+//null
+//undefined
+
+/* Number */
+const no1:Number = 12;
+console.log(no1);
+
+/* String */
+const name1:String = "Moditha";
+console.log(name1);
+
+/* Null */
+const age:Number = null;
+console.log(age);
