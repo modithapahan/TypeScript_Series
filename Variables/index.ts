8b57c317b00111f8b3b01d95f7a58eb32f990cea
modithapahan <modithapahan0@gmail.com>
2022-09-18 21:16:10 +0530

    Type Script

--- Variables/index.ts
+++ Variables/index.ts
@@ -1,4 +1,4 @@
-let name1:string = "moditha";
+let name1: string = "moditha";
 
 let score1:number = 50;
 let score2:number = 60;
@@ -7,3 +7,16 @@ console.log(name1);
 
 const sum = score1 + score2;
 console.log('Sum is ' + sum);
+
+console.log("--------------------------------------");
+
+let number1 = 12;
+let number2: string = <string><any>number1;
+console.log(typeof (number2));
+
+let name22 = "modi";
+let name23: number = <number><any>name22;
+console.log(typeof(name23));
+
+
+console.log("--------------------------------------");
